5b85d970701e4d1084eab094c2dda706ddc0dac7
bharathboji <madhusudhan34116@gmail.com>
2024-02-21 17:36:25 +0530

    added a one example of class and objects

---
+++ claassAnd objectStudents.py
@@ -0,0 +1,20 @@
+
+class Staff():
+    firstName= "bala "
+    lastName= "krishna "
+    #def __init__(self,first,second):
+     #self.firstName= first
+      #self.lastName= second
+
+    def getFullName(self):
+        return self.firstName+" "+self.lastName
+    
+Anusha=Staff()
+print(Anusha.getFullName())
+
+
+
+ 
+
+
+     
